776a78ffb47c342aa2f4080da8b37b4d7f0539ac
zizifn <1803942+zizifn@users.noreply.github.com>
2024-12-01 16:39:11 +0800

    add relative path

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
-  <router-view/>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/accessibility">accessibility</RouterLink>
+  </nav>
+
+  <router-view />
 </template>
 
 <style lang="scss">
-   @use './assets/style/common.scss';
+@use './assets/style/common.scss';
 </style>
